1553e90ed8d1b1adfe5161ab6c01b211d47a1f55
rabisonshakya1234 <111277590+rabisonshakya1234@users.noreply.github.com>
2022-12-30 07:54:26 +0545

    back-end

---
+++ app.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const app = express();
+app.use(express.json());
+
+const cors = require("cors");
+
+
+app.use(cors());
+
+// includes
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+
+dotenv.config();
+
+// router include
+const userRoute = require("./app/routes/route.user");
+const categoryRoute = require("./app/routes/route.category");
+const authRoute = require("./app/routes/route.auth");
+
+const verifyJwt = require("./app/middleware/verifyJWT");
+
+mongoose.connect(
+  process.env.DATABASE_URL,
+  { useNewUrlParser: true},
+
+  () => {
+    console.log("Connected to DB!");
+  }
+);
+mongoose.connection.on("connected", (err, res) => {
+    console.log("MongoDB connected successfully!");
+  });
+
+
+console.log(process.env.DATABASE_URL);
+
+
+
+app.use("/api", authRoute);
+app.use("/api/user", userRoute);
+
+// protected routes
+app.use(verifyJwt);
+app.use("/api/category", categoryRoute);
+
+// app listen
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port ${process.env.PORT}`);
+});
